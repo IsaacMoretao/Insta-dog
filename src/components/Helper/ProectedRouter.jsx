1a0586fc81d09f52b5570eacd4520cfb23f7ef4f
Isaac Moretão <isaachanam@gmail.com>
2022-09-09 16:27:18 -0300

    Commit

--- src/components/Helper/ProectedRouter.jsx
+++ src/components/Helper/ProectedRouter.jsx
@@ -4,7 +4,7 @@ import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
   const { login } = React.useContext(UserContext);
-  return login ? children : <Navigate to="/login" />;
+  return login ? children : <Navigate to="/Insta-dog/Loguin" />;
 };
 
 export default ProtectedRoute;
